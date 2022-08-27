5b9cd687902faad1a3a04e27a0a0ba828aabae13
Kiryl Anokhin <alyrik@gmail.com>
2022-08-27 10:58:38 +0200

    Adjust styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import RecentSearches from './components/RecentSearches/RecentSearches';
 
 function App() {
   return (
-    <div className="grid grid-cols-4 grid-rows-1 gap-4 h-screen p-4">
+    <div className="grid grid-cols-4 grid-rows-1 gap-4 min-h-screen p-4">
       <div className="col-span-1 h-full overflow-auto">
         <RecentSearches />
       </div>
@@ -17,13 +17,13 @@ function App() {
             <CurrentLocation />
           </div>
           <div className="row-span-4">
-            <div className="grid grid-rows-4 gap-4 h-full">
-              <div className="row-span-1 grid grid-cols-5">
+            <div className="grid grid-rows-6 gap-4 h-full">
+              <div className="row-span-1 grid grid-cols-5 items-end">
                 <div className="col-span-3">
                   <IpSearchBox />
                 </div>
               </div>
-              <div className="row-span-3">
+              <div className="row-span-5">
                 <FoundLocation />
               </div>
             </div>
--- src/components/CurrentLocation/CurrentLocation.tsx
+++ src/components/CurrentLocation/CurrentLocation.tsx
@@ -7,28 +7,31 @@ export const CurrentLocation: FC = () => {
   const { data: currentLocationData, isError } = useGetCurrentLocation();
 
   return (
-    <div className="grid grid-cols-5 grid-rows-1 gap-4 h-full">
-      <div className="col-span-3">
-        {currentLocationData && (
-          <CustomMap
-            latitude={currentLocationData.latitude}
-            longitude={currentLocationData.longitude}
-            markerText="That's your location based on your IP. No one said it would match your physical location :)"
-          />
-        )}
-      </div>
-      <div className="col-span-2">
-        {currentLocationData && (
-          <LocationDescription
-            ip={currentLocationData.ip}
-            country={currentLocationData.country}
-            region={currentLocationData.region}
-            city={currentLocationData.city}
-            latitude={currentLocationData.latitude}
-            longitude={currentLocationData.longitude}
-            organization={currentLocationData.organization}
-          />
-        )}
+    <div>
+      <h2 className="font-bold mb-2">Your current location:</h2>
+      <div className="grid grid-cols-5 grid-rows-1 gap-4 h-full">
+        <div className="col-span-3">
+          {currentLocationData && (
+            <CustomMap
+              latitude={currentLocationData.latitude}
+              longitude={currentLocationData.longitude}
+              markerText="That's your location based on your IP. No one said it would match your physical location :)"
+            />
+          )}
+        </div>
+        <div className="col-span-2">
+          {currentLocationData && (
+            <LocationDescription
+              ip={currentLocationData.ip}
+              country={currentLocationData.country}
+              region={currentLocationData.region}
+              city={currentLocationData.city}
+              latitude={currentLocationData.latitude}
+              longitude={currentLocationData.longitude}
+              organization={currentLocationData.organization}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
